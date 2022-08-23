3d8d04bb2f9ae6a2c0eaa980cc87a1ad38139df6
dtrojanowski2 <dtrojanowski2@huskers.unl.edu>
2022-08-23 14:01:45 +0000

    oops almost forgot to actually write a hello world file

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * almost forgot to actually write a hello world file oops
+ * Author: Daniel Trojanowski
+ * Date: 8/23/2022
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World!\n");
+
+    return 0;
+}
